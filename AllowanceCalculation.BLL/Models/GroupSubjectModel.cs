d5ef64ce2e281d96c2c424f42b46aecf752f313d
spiraspira <zabornik5350@gmail.com>
2023-11-05 22:37:48 +0300

    Add Models

---
+++ AllowanceCalculation.BLL/Models/GroupSubjectModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AllowanceCalculation.BLL.Models;
+
+public class GroupSubjectModel
+{
+	public int Id { get; set; }
+
+	public int GroupId { get; set; }
+
+	public GroupModel? Group { get; set; }
+
+	public int SubjectId { get; set; }
+
+	public SubjectModel? Subject { get; set; }
+}
