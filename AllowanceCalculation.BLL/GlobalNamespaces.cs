e11d645f47dd561f675836e321d8c080ce8f5f3a
spiraspira <zabornik5350@gmail.com>
2023-11-05 22:42:13 +0300

    Add mapper;

---
+++ AllowanceCalculation.BLL/GlobalNamespaces.cs
@@ -0,0 +1,5 @@
+global using AllowanceCalculation.DAL.Entities;
+
+global using AllowanceCalculation.BLL.Models;
+
+global using AutoMapper;
---
+++ AllowanceCalculation.BLL/Mapper/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace AllowanceCalculation.BLL.Mapper;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<Group, GroupModel>().ReverseMap();
+		CreateMap<GroupSubject, GroupSubjectModel>().ReverseMap();
+		CreateMap<StudentGrade, StudentGradeModel>().ReverseMap();
+		CreateMap<Student, StudentModel>().ReverseMap();
+		CreateMap<Subject, SubjectModel>().ReverseMap();
+	}
+}
