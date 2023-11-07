d1e0c5fc112661712a53dd2ac7ddeb2c1b8bad2d
spiraspira <zabornik5350@gmail.com>
2023-11-08 01:10:24 +0300

    Refactored DAL/Entities

--- AllowanceCalculation.DAL/Entities/Subject.cs
+++ AllowanceCalculation.DAL/Entities/Subject.cs
@@ -6,7 +6,7 @@ public class Subject : EntityWithId
 
 	public bool? IsPassFailEvaluated { get; set; }
 
-	public List<GroupSubject> GroupSubjects { get; set; } = new List<GroupSubject>();
+	public List<GroupSubject> GroupSubjects { get; set; } = new();
 
-	public List<StudentGrade> StudentGrades { get; set; } = new List<StudentGrade>();
+	public List<StudentGrade> StudentGrades { get; set; } = new();
 }
