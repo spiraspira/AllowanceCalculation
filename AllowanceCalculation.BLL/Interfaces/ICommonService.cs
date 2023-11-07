f2abbefbcc2ec74097f003204db72f39af203161
spiraspira <zabornik5350@gmail.com>
2023-11-07 12:04:24 +0300

    Add CommonService

---
+++ AllowanceCalculation.BLL/Interfaces/ICommonService.cs
@@ -0,0 +1,14 @@
+﻿namespace AllowanceCalculation.BLL.Interfaces;
+
+public interface ICommonService<TModel> where TModel : class
+{
+	Task<TModel> Create(TModel model);
+
+	Task<IEnumerable<TModel>> GetAll();
+
+	Task<TModel> Get(int id);
+
+	Task<TModel> Update(TModel model);
+
+	Task Delete(int id);
+}
--- AllowanceCalculation.BLL/Mapper/MappingProfile.cs
+++ AllowanceCalculation.BLL/Mapper/MappingProfile.cs
@@ -5,9 +5,13 @@ public class MappingProfile : Profile
 	public MappingProfile()
 	{
 		CreateMap<Group, GroupModel>().ReverseMap();
+
 		CreateMap<GroupSubject, GroupSubjectModel>().ReverseMap();
+
 		CreateMap<StudentGrade, StudentGradeModel>().ReverseMap();
+
 		CreateMap<Student, StudentModel>().ReverseMap();
+
 		CreateMap<Subject, SubjectModel>().ReverseMap();
 	}
 }
